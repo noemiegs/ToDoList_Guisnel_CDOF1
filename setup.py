0ee72db3ba77a692d5c172286d0a23cc3034f638
noemiegs <113913066+noemiegs@users.noreply.github.com>
2024-01-22 14:10:23 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{ToDoList_Guisnel_CDOF1}',
+ version='1.0',
+ author='{noemiegs}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
